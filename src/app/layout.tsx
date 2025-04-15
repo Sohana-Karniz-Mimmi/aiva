4c29af9faf22bbbb2196d3a186e2a430227081fc
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2025-04-15 12:35:20 +0600

    setup font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Manrope } from "next/font/google";
 
-const manrope = Manrope({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-manrope",
-  display: "swap",
-});
 export const metadata: Metadata = {
   title: "AIVA",
   description: "Explore the intersection of creativity and technology. Turning ideas into innovative digital experiences.",
@@ -23,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={manrope.className}>{children}</body>
+      <body >{children}</body>
     </html>
   );
 }
--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <section className='custom_container'>
+        <section className='custom_container '>
             This is Home page
+            <h1 className='font-polySans font-bold text-[50px]'>Empower</h1>
+            <h1 className='font-polySans font-bold text-[44px] leading-[52px] tracking-[0.66px]'>+1 888 502 5576</h1>
+            <p className='font-general font-normal text-[16px]'>Meet AILANA</p>
         </section>
     );
 };
